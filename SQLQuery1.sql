6a3e45acbb66e2879c39c5f722efa4fdeaaa3361
AdrianBSilva <38699788+AdrianBSilva@users.noreply.github.com>
2018-06-21 16:53:09 -0300

    Fim Do Exercicio 01

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿drop table pessoas;
+create table pessoas(
+	nome				varchar(100),
+	cpf					varchar(14),
+	rg					varchar(12),
+	data_de_Nascimento  date,
+	idade				tinyint
+);
+
+insert into pessoas values('Eric Bryan Enrico Vieira','699.734.958-70', '90.754.276-0', '1989-05-28', 29);
+insert into pessoas values('Samuel Ruan Dias', '802.790.194', '55.318.691-7', '1988-02-08', 30);
+insert into pessoas values('Fábio Benjamin Souza', '522.445.349-60', '54.715.232-2', '1970-03-22', 48);
+insert into pessoas values('Elias Miguel Aparicio','293.928.821-65','12.680.444-8','1986-06-07',32);
+insert into pessoas values('Alexandre Gustavo Cardoso','293.928.821-65','12.680.444-8','2003-04-07',15);
+insert into pessoas values('Kauê Lucca Duarte','943.163.990-47','46.977.509-9','1965-06-15', 53);
+insert into pessoas values('Marcos Bryan Guilherme Mendes', '955.129.235-95', '84.050.077-4', '2015-02-06', 3);
+insert into pessoas values('Benedito Pedro Carlos da Mota', '510.505.472-50', '43.588.124-3', '1981-04-19', 37);
+insert into pessoas values('Luís Igor Barbosa', '370.570.311-06', '40.651.407-0', '2013-05-06', 5);
+insert into pessoas values('José Heitor Ramos', '675.624.925-81', '83.962.258-2', '1965-04-11', 53);
+insert into pessoas values('Lucca Tiago Galvão', '788.124.508-57', '3.293.335-6', '1977-06-17', 41);
+insert into pessoas values('Tiago Edson Oliveira', '389.969.352-39', '8.198.446-9', '1977-06-09', 41);
+insert into pessoas values('Enrico Julio Fábio Martins', '529.078.166-83', '99.886.242-3', '2017-03-11', 1);
+insert into pessoas values('Oliver Victor Benjamin da Mota', '798.556.805-02', '5.103.578-9', '1985-03-27',33);
+insert into pessoas values('Bryan Caio Lopes', '213.217.201-30', '12.949.454-9', '1970-06-18', 48);

c4d6ba9d1d8bfed6aa897125e009aeb03c6f70bb
AdrianBSilva <38699788+AdrianBSilva@users.noreply.github.com>
2018-06-22 13:57:26 -0300

    exercicio01 e 02

---
+++ exercicio03.sql
@@ -0,0 +1,20 @@
+﻿drop table champions
+create table champions(
+	nome			varchar(100),
+	descricao		varchar(100),
+	habilidade_1	varchar(100),
+	habilidade_2	varchar(100),
+	habilidade_3	varchar(100),
+	habilidade_4	varchar(100),
+	habilidade_5	varchar(100)
+);
+
+insert into champions values('Katarina', 'A Lâmina snaguinaria', 'Voracidade', 'Lâmina Saltitante~','Preparação','Shunpo', 'Lótus da Morte');
+insert into champions values(nome)();
+insert into champions values();
+insert into champions values();
+insert into champions values();
+insert into champions values();
+insert into champions values();
+insert into champions values();
+insert into champions values();
